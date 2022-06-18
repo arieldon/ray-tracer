14516a4abf51dbbea868aeda184d48c12466101a
Ariel Don <ariel@arieldon.com>
2022-06-17 21:56:30 -0500

    Use Zig's native Vector type for type Tuple

--- src/main.zig
+++ src/main.zig
@@ -2,173 +2,110 @@ const std = @import("std");
 const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 
-const Tuple = struct {
-    x: f32,
-    y: f32,
-    z: f32,
-    w: f32,
+const Tuple = @Vector(4, f32);
+const Vector = Tuple;
+const Point = Tuple;
 
-    pub fn init(x: f32, y: f32, z: f32, w: f32) Tuple {
-        return Tuple{
-            .x = x,
-            .y = y,
-            .z = z,
-            .w = w,
-        };
-    }
+pub fn tuple(x: f32, y: f32, z: f32, w: f32) Tuple {
+    return .{ x, y, z, w };
+}
 
-    pub fn initPoint(x: f32, y: f32, z: f32) Tuple {
-        return Tuple{
-            .x = x,
-            .y = y,
-            .z = z,
-            .w = 1,
-        };
-    }
+pub fn point(x: f32, y: f32, z: f32) Point {
+    return .{ x, y, z, 1 };
+}
 
-    pub fn initVector(x: f32, y: f32, z: f32) Tuple {
-        return Tuple{
-            .x = x,
-            .y = y,
-            .z = z,
-            .w = 0,
-        };
-    }
+pub fn vector(x: f32, y: f32, z: f32) Vector {
+    return .{ x, y, z, 0 };
+}
 
-    pub fn isPoint(self: Tuple) bool {
-        return self.w == 1;
-    }
+pub fn isPoint(t: Tuple) bool {
+    return t[3] == 1;
+}
 
-    pub fn isVector(self: Tuple) bool {
-        return self.w == 0;
-    }
-
-    pub fn add(self: Tuple, other: Tuple) Tuple {
-        return Tuple{
-            .x = self.x + other.x,
-            .y = self.y + other.y,
-            .z = self.z + other.z,
-            .w = self.w + other.w,
-        };
-    }
-
-    pub fn sub(self: Tuple, other: Tuple) Tuple {
-        return Tuple{
-            .x = self.x - other.x,
-            .y = self.y - other.y,
-            .z = self.z - other.z,
-            .w = self.w - other.w,
-        };
-    }
-
-    pub fn neg(self: Tuple) Tuple {
-        return Tuple{
-            .x = -self.x,
-            .y = -self.y,
-            .z = -self.z,
-            .w = -self.w,
-        };
-    }
-
-    pub fn mul(self: Tuple, other: f32) Tuple {
-        return Tuple{
-            .x = self.x * other,
-            .y = self.y * other,
-            .z = self.z * other,
-            .w = self.w * other,
-        };
-    }
-
-    pub fn div(self: Tuple, other: f32) Tuple {
-        return Tuple{
-            .x = self.x / other,
-            .y = self.y / other,
-            .z = self.z / other,
-            .w = self.w / other,
-        };
-    }
-};
+pub fn isVector(t: Tuple) bool {
+    return t[3] == 0;
+}
 
 test "a tuple with w=1.0 is a point" {
-    const a = Tuple.init(4.3, -4.2, 3.1, 1.0);
+    const a = tuple(4.3, -4.2, 3.1, 1.0);
 
-    try expect(a.x == 4.3);
-    try expect(a.y == -4.2);
-    try expect(a.z == 3.1);
-    try expect(a.w == 1.0);
+    try expect(a[0] == 4.3);
+    try expect(a[1] == -4.2);
+    try expect(a[2] == 3.1);
+    try expect(a[3] == 1.0);
 
-    try expect(a.isPoint() == true);
-    try expect(a.isVector() == false);
+    try expect(isPoint(a) == true);
+    try expect(isVector(a) == false);
 }
 
 test "a tuple with w=0 is a vector" {
-    const a = Tuple.init(4.3, -4.2, 3.1, 0.0);
+    const a = tuple(4.3, -4.2, 3.1, 0.0);
 
-    try expect(a.x == 4.3);
-    try expect(a.y == -4.2);
-    try expect(a.z == 3.1);
-    try expect(a.w == 0.0);
+    try expect(a[0] == 4.3);
+    try expect(a[1] == -4.2);
+    try expect(a[2] == 3.1);
+    try expect(a[3] == 0.0);
 
-    try expect(a.isPoint() == false);
-    try expect(a.isVector() == true);
+    try expect(isPoint(a) == false);
+    try expect(isVector(a) == true);
 }
 
 test "point() creates tuples with w=1" {
-    const p = Tuple.initPoint(4, -4, 3);
-    try expectEqual(p, Tuple.init(4, -4, 3, 1));
+    const p = point(4, -4, 3);
+    try expectEqual(p, tuple(4, -4, 3, 1));
 }
 
 test "vector() creates tuples with w=0" {
-    const v = Tuple.initVector(4, -4, 3);
-    try expectEqual(v, Tuple.init(4, -4, 3, 0));
+    const v = vector(4, -4, 3);
+    try expectEqual(v, tuple(4, -4, 3, 0));
 }
 
 test "adding two tuples" {
-    const a1 = Tuple.init(3, -2, 5, 1);
-    const a2 = Tuple.init(-2, 3, 1, 0);
-    try expectEqual(Tuple.add(a1, a2), Tuple.init(1, 1, 6, 1));
+    const a1 = tuple(3, -2, 5, 1);
+    const a2 = tuple(-2, 3, 1, 0);
+    try expectEqual(a1 + a2, tuple(1, 1, 6, 1));
 }
 
 test "subtracting two points" {
-    const p1 = Tuple.initPoint(3, 2, 1);
-    const p2 = Tuple.initPoint(5, 6, 7);
-    try expectEqual(Tuple.sub(p1, p2), Tuple.initVector(-2, -4, -6));
+    const p1 = point(3, 2, 1);
+    const p2 = point(5, 6, 7);
+    try expectEqual(p1 - p2, vector(-2, -4, -6));
 }
 
 test "subtracting a vector from a point" {
-    const p = Tuple.initPoint(3, 2, 1);
-    const v = Tuple.initVector(5, 6, 7);
-    try expectEqual(Tuple.sub(p, v), Tuple.initPoint(-2, -4, -6));
+    const p = point(3, 2, 1);
+    const v = vector(5, 6, 7);
+    try expectEqual(p - v, point(-2, -4, -6));
 }
 
 test "subtracting two vectors" {
-    const v1 = Tuple.initVector(3, 2, 1);
-    const v2 = Tuple.initVector(5, 6, 7);
-    try expectEqual(Tuple.sub(v1, v2), Tuple.initVector(-2, -4, -6));
+    const v1 = vector(3, 2, 1);
+    const v2 = vector(5, 6, 7);
+    try expectEqual(v1 - v2, vector(-2, -4, -6));
 }
 
 test "subtracting a vector from the zero vector" {
-    const zero = Tuple.initVector(0, 0, 0);
-    const v = Tuple.initVector(1, -2, 3);
-    try expectEqual(Tuple.sub(zero, v), Tuple.initVector(-1, 2, -3));
+    const zero = vector(0, 0, 0);
+    const v = vector(1, -2, 3);
+    try expectEqual(zero - v, vector(-1, 2, -3));
 }
 
 test "negating a tuple" {
-    const a = Tuple.init(1, -2, 3, -4);
-    try expectEqual(Tuple.neg(a), Tuple.init(-1, 2, -3, 4));
+    const a = tuple(1, -2, 3, -4);
+    try expectEqual(-a, tuple(-1, 2, -3, 4));
 }
 
 test "multiplying a tuple by a scalar" {
-    const a = Tuple.init(1, -2, 3, -4);
-    try expectEqual(Tuple.mul(a, 3.5), Tuple.init(3.5, -7, 10.5, -14));
+    const a = tuple(1, -2, 3, -4);
+    try expectEqual(a * @splat(4, @as(f32, 3.5)), tuple(3.5, -7, 10.5, -14));
 }
 
 test "multiplying a tuple by a fraction" {
-    const a = Tuple.init(1, -2, 3, -4);
-    try expectEqual(Tuple.mul(a, 0.5), Tuple.init(0.5, -1, 1.5, -2));
+    const a = tuple(1, -2, 3, -4);
+    try expectEqual(a * @splat(4, @as(f32, 0.5)), tuple(0.5, -1, 1.5, -2));
 }
 
 test "dividing a tuple by a scalar" {
-    const a = Tuple.init(1, -2, 3, -4);
-    try expectEqual(Tuple.div(a, 2), Tuple.init(0.5, -1, 1.5, -2));
+    const a = tuple(1, -2, 3, -4);
+    try expectEqual(a / @splat(4, @as(f32, 2)), tuple(0.5, -1, 1.5, -2));
 }
