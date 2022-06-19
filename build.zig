5bccd6d08e9eba04324d81466c41599623fc3427
Ariel Don <ariel@arieldon.com>
2022-06-19 09:34:23 -0500

    Separate files and update build system accordingly

--- build.zig
+++ build.zig
@@ -2,14 +2,18 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
 
-    const lib = b.addStaticLibrary("raytracer", "src/main.zig");
-    lib.setBuildMode(mode);
-    lib.install();
+    const tests = b.addTest("src/main.zig");
+    tests.setBuildMode(mode);
+    tests.setTarget(target);
+    tests.addPackage(ray_tracer);
 
-    const main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&tests.step);
 }
+
+pub const ray_tracer = std.build.Pkg{
+    .name = "ray-tracer",
+    .path = std.build.FileSource.relative("src/main.zig"),
+};
