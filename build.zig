309a95e7a2b7bf22ac43d460b779aaaabe6be932
Ariel Don <ariel@arieldon.com>
2022-06-23 21:44:57 -0500

    Write a sample that produces a red circle

--- build.zig
+++ build.zig
@@ -11,6 +11,17 @@ pub fn build(b: *std.build.Builder) void {
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&tests.step);
+
+    // Construct the only sample so far.
+    const exe = b.addExecutable("red-sphere", "samples/red_sphere.zig");
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+
+    // Link the library.
+    exe.addPackage(ray_tracer);
+
+    // Build the sample.
+    exe.install();
 }
 
 pub const ray_tracer = std.build.Pkg{
--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,11 @@
 const std = @import("std");
 
-pub const canvas = @import("canvas.zig");
-pub const intersection = @import("intersection.zig");
-pub const matrix = @import("matrix.zig");
+pub const cnv = @import("canvas.zig");
+pub const int = @import("intersection.zig");
+pub const mat = @import("matrix.zig");
 pub const ray = @import("ray.zig");
-pub const sphere = @import("sphere.zig");
-pub const tuple = @import("tuple.zig");
+pub const sph = @import("sphere.zig");
+pub const tup = @import("tuple.zig");
 
 test {
     std.testing.refAllDecls(@This());
