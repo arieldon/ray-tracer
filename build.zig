d47d345b1ee7b6f0fa90de926ed32735b99712fe
Ariel Don <ariel@arieldon.com>
2022-06-25 11:41:47 -0500

    Create sample that outputs a shaded purple sphere

--- build.zig
+++ build.zig
@@ -7,21 +7,25 @@ pub fn build(b: *std.build.Builder) void {
     const tests = b.addTest("src/main.zig");
     tests.setBuildMode(mode);
     tests.setTarget(target);
-    tests.addPackage(ray_tracer);
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&tests.step);
 
-    // Construct the only sample so far.
-    const exe = b.addExecutable("red-sphere", "samples/red_sphere.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
+    inline for (.{
+        .{ .bin_name = "red-sphere", .source_path = "samples/red_sphere.zig" },
+        .{ .bin_name = "shaded-sphere", .source_path = "samples/shaded_sphere.zig" },
+    }) |sample| {
+        // Construct the sample.
+        const exe = b.addExecutable(sample.bin_name, sample.source_path);
+        exe.setTarget(target);
+        exe.setBuildMode(mode);
 
-    // Link the library.
-    exe.addPackage(ray_tracer);
+        // Link the library.
+        exe.addPackage(ray_tracer);
 
-    // Build the sample.
-    exe.install();
+        // Build the sample.
+        exe.install();
+    }
 }
 
 pub const ray_tracer = std.build.Pkg{
---
+++ samples/shaded_sphere.zig
@@ -0,0 +1,69 @@
+const std = @import("std");
+const rt = @import("ray-tracer");
+
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+
+    // Define constants for the scene.
+    const canvas_pixels = 400;
+    const wall_z = 10;
+    const wall_size = 7.0;
+    const half = wall_size / 2.0;
+    const pixel_size = wall_size / @intToFloat(f32, canvas_pixels);
+    const ray_origin = rt.tup.point(0, 0, -5);
+
+    // Create a light source above, behind, and to the left of the eye.
+    const light_position = rt.tup.point(-10, 10, -10);
+    const light_color = rt.cnv.color(1, 1, 1);
+    const light = rt.lht.point_light(light_position, light_color);
+
+    // Create a purple sphere.
+    var sphere = rt.sph.sphere();
+    sphere.material.color = rt.cnv.color(1, 0.2, 1);
+
+    // Create a canvas.
+    var canvas = try rt.cnv.canvas(allocator, canvas_pixels, canvas_pixels);
+    defer canvas.deinit();
+
+    // Allocate a list to store intersections.
+    var intersections = std.ArrayList(rt.int.Intersection).init(allocator);
+    defer intersections.deinit();
+
+    var y: u32 = 0;
+    while (y < canvas_pixels) : (y += 1) {
+        const world_y = half - pixel_size * @intToFloat(f32, y);
+
+        var x: u32 = 0;
+        while (x < canvas_pixels) : (x += 1) {
+            const world_x = -half + pixel_size * @intToFloat(f32, x);
+            const position = rt.tup.point(world_x, world_y, wall_z);
+            const ray = rt.ray.ray(ray_origin, rt.tup.normalize(position - ray_origin));
+
+            // Calculate ray-sphere intersections. Upon intersection, compute
+            // pixel color based on the scene's lighting.
+            try rt.sph.intersect(&intersections, sphere, ray);
+            if (rt.int.hit(&intersections)) |hit| {
+                const point = rt.ray.position(ray, hit.t);
+                const normal = rt.sph.normal_at(hit.object, point);
+                const eye = -ray.direction;
+                const color = rt.mtl.lighting(
+                    hit.object.material,
+                    light,
+                    point,
+                    eye,
+                    normal);
+                canvas.writePixel(x, y, color);
+            }
+
+            // Clear list of intersections.
+            intersections.items.len = 0;
+        }
+    }
+
+    // Create a new file in the current working directory.
+    const file = try std.fs.cwd().createFile("shaded_sphere.ppm", .{});
+    defer file.close();
+
+    // Write contents of canvas as a viewable PPM file.
+    try canvas.toPPM(file.writer());
+}
