e1c064ee32e6e9edb9ecb12db7ec76af899ba2f9
Ariel Don <ariel@arieldon.com>
2022-07-31 16:15:35 -0500

    Handle `g` commands in OBJ files
    `g` commands specify group names for the faces that follow them.

--- src/obj_file.zig
+++ src/obj_file.zig
@@ -1,18 +1,24 @@
 const std = @import("std");
+const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 const tup = @import("tuple.zig");
 const tri = @import("triangle.zig");
 
 const Vertex = tup.Point;
 const Face = tri.Triangle;
+const Group = std.ArrayList(Face);
 
 const ParserCommand = enum {
     /// Define a new vertex.
     vertex,
 
     /// Define a new face, where a face consists of three points that form a
-    /// triangle.
+    /// triangle or four or more points that form a polygon. The parser then
+    /// converts the latter into triangles.
     face,
+
+    /// Specify a group name for the next face.
+    group,
 };
 
 const ParserError = error {
@@ -20,13 +26,24 @@ const ParserError = error {
 };
 
 const ObjFile = struct {
+    allocator: std.mem.Allocator,
+
     vertices: std.ArrayList(Vertex),
     faces: std.ArrayList(Face),
+    groups: std.StringHashMap(Group),
+
     number_of_ignored_commands: usize = 0,
 
     pub fn deinit(self: *ObjFile) void {
         self.vertices.deinit();
         self.faces.deinit();
+
+        var group_iter = self.groups.iterator();
+        while (group_iter.next()) |named_group| {
+            self.allocator.free(named_group.key_ptr.*);
+            named_group.value_ptr.deinit();
+        }
+        self.groups.deinit();
     }
 };
 
@@ -34,8 +51,10 @@ pub fn parseObjFile(allocator: std.mem.Allocator, file: std.fs.File) !ObjFile {
     // FIXME What if there are two spaces in between parameters?
 
     var obj = ObjFile{
+        .allocator = allocator,
         .vertices = std.ArrayList(Vertex).init(allocator),
         .faces = std.ArrayList(Face).init(allocator),
+        .groups = std.StringHashMap(Group).init(allocator),
     };
     errdefer obj.deinit();
 
@@ -47,6 +66,7 @@ pub fn parseObjFile(allocator: std.mem.Allocator, file: std.fs.File) !ObjFile {
     var contents = try file.reader().readAllAlloc(allocator, std.math.maxInt(usize));
     defer allocator.free(contents);
 
+    var current_group_name: ?[]const u8 = null;
     var line_start: usize = 0;
     iterate_contents: for (contents) |contents_character, contents_index| {
         if (contents_character == '\n') {
@@ -55,8 +75,12 @@ pub fn parseObjFile(allocator: std.mem.Allocator, file: std.fs.File) !ObjFile {
             // marker after `continue` statements as well.
             defer line_start = contents_index + 1;
 
-            // Skip blank lines.
-            if (contents_index - line_start == 0) continue :iterate_contents;
+            // Handle blank lines.
+            if (contents_index - line_start == 0) {
+                // Blank lines end groups of faces.
+                current_group_name = null;
+                continue :iterate_contents;
+            }
 
             // Increment copy of index in contents to include newline character.
             const line_end = contents_index + 1;
@@ -76,6 +100,8 @@ pub fn parseObjFile(allocator: std.mem.Allocator, file: std.fs.File) !ObjFile {
                 command = .vertex;
             } else if (std.mem.eql(u8, command_string, "f")) {
                 command = .face;
+            } else if (std.mem.eql(u8, command_string, "g")) {
+                command = .group;
             } else {
                 // Ignore invalid or unsupported commands silently.
                 obj.number_of_ignored_commands += 1;
@@ -102,6 +128,19 @@ pub fn parseObjFile(allocator: std.mem.Allocator, file: std.fs.File) !ObjFile {
 
                     if (vertex_component != 3) return ParserError.InvalidCommandArity;
                     try obj.vertices.append(vertex);
+
+                    // Assign face to group if specified.
+                    if (current_group_name) |group_name| {
+                        const entry = try obj.groups.getOrPut(try allocator.dupe(u8, group_name));
+                        if (!entry.found_existing) {
+                            // Initialize array list for new sequence of faces
+                            // in this group.
+                            entry.value_ptr.* = Group.init(allocator);
+                        }
+
+                        // Append face to named group.
+                        try entry.value_ptr.append(obj.faces.items[obj.faces.items.len - 1]);
+                    }
                 },
                 .face => {
                     var vertex_indeces = try std.ArrayList(usize).initCapacity(allocator, 3);
@@ -127,7 +166,29 @@ pub fn parseObjFile(allocator: std.mem.Allocator, file: std.fs.File) !ObjFile {
                             obj.vertices.items[vertex_indeces.items[1]],
                             obj.vertices.items[vertex_indeces.items[2]]));
                     }
-                }
+
+                    // Assign face to group if specified.
+                    if (current_group_name) |group_name| {
+                        const entry = try obj.groups.getOrPut(try allocator.dupe(u8, group_name));
+                        if (!entry.found_existing) {
+                            // Initialize array list for new sequence of faces
+                            // in this group.
+                            entry.value_ptr.* = Group.init(allocator);
+                        }
+
+                        // Append face to named group.
+                        try entry.value_ptr.append(obj.faces.items[obj.faces.items.len - 1]);
+                    }
+                },
+                .group => {
+                    for (line[literal_start..]) |literal_character, literal_index| {
+                        if (literal_character == ' ' or literal_character == '\n') {
+                            literal_end = command_end + 1 + literal_index;
+                            current_group_name = line[literal_start..literal_end];
+                            break;
+                        }
+                    } else return ParserError.InvalidCommandArity;
+                },
             }
         }
     }
@@ -218,4 +279,29 @@ test "triangulating polygons" {
     try expectEqual(t2.p0, obj.vertices.items[1]);
     try expectEqual(t2.p1, obj.vertices.items[4]);
     try expectEqual(t2.p2, obj.vertices.items[5]);
+}
+
+test "triangles in groups" {
+    const file = try std.fs.cwd().openFile("triangles.obj", .{});
+    defer file.close();
+
+    var obj = try parseObjFile(std.testing.allocator, file);
+    defer obj.deinit();
+
+    try expect(obj.groups.contains("FirstGroup"));
+    try expect(obj.groups.contains("SecondGroup"));
+
+    const g0 = obj.groups.get("FirstGroup").?;
+    const g1 = obj.groups.get("SecondGroup").?;
+
+    const t0 = g0.items[0];
+    const t1 = g1.items[0];
+
+    try expectEqual(t0.p0, obj.vertices.items[1]);
+    try expectEqual(t0.p1, obj.vertices.items[2]);
+    try expectEqual(t0.p2, obj.vertices.items[3]);
+
+    try expectEqual(t1.p0, obj.vertices.items[1]);
+    try expectEqual(t1.p1, obj.vertices.items[3]);
+    try expectEqual(t1.p2, obj.vertices.items[4]);
 }
