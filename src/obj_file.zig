f5f6d65662e3c52385459b6e678f13bb22901fa3
Ariel Don <ariel@arieldon.com>
2022-08-06 16:01:29 -0500

    Rewrite OBJ file parser

--- src/obj_file.zig
+++ src/obj_file.zig
@@ -25,7 +25,7 @@ const ParserCommand = enum {
 };
 
 const ParserError = error {
-    InvalidCommandArity,
+    InvalidCommand,
 };
 
 const ObjFile = struct {
@@ -41,9 +41,6 @@ const ObjFile = struct {
     /// Store faces declared under a named group in a separate list for this
     /// group.
     groups: std.StringHashMap(Group),
-
-    /// Track number of unsuppored commands encountered while parsing.
-    number_of_ignored_commands: usize = 0,
 
     /// Convert the parsed result of an OBJ file to a group of shapes that the
     /// ray tracer can render.
@@ -80,8 +77,6 @@ const ObjFile = struct {
 };
 
 pub fn parseObjFile(allocator: std.mem.Allocator, file: std.fs.File) !ObjFile {
-    // FIXME What if there are two spaces in between parameters?
-
     var obj = ObjFile{
         .allocator = allocator,
         .vertices = std.ArrayList(Vertex).init(allocator),
@@ -99,83 +94,31 @@ pub fn parseObjFile(allocator: std.mem.Allocator, file: std.fs.File) !ObjFile {
     defer allocator.free(contents);
 
     var group_name: ?[]const u8 = null;
-    var line_start: usize = 0;
-    iterate_contents: for (contents) |contents_character, contents_index| {
-        if (contents_character == '\n') {
-            // At the end of this block, update start of line marker for
-            // interpretation of next command. Use `defer` to update the this
-            // marker after `continue` statements as well.
-            defer line_start = contents_index + 1;
+    var lines = std.mem.tokenize(u8, contents, "\n");
+    while (lines.next()) |line| {
+        var literals = std.mem.tokenize(u8, line, " ");
+        while (literals.next()) |literal| {
+            // This parser only supports commands v, f, and g -- all of which
+            // are of length 1.
+            if (literal.len > 1) return ParserError.InvalidCommand;
 
-            // Handle blank lines.
-            if (contents_index - line_start == 0) continue :iterate_contents;
-
-            // Increment copy of index in contents to include newline character.
-            const line_end = contents_index + 1;
-            const line = contents[line_start..line_end];
-
-            // Define variables and separate command.
-            var command_start: usize = 0;
-            var command_end: usize =
-                for (line) |literal_character, line_index| {
-                    if (literal_character == ' ') break line_index;
-                } else 0;
-            var command_string = line[command_start..command_end];
-
-            // Parse command.
-            var command: ParserCommand = undefined;
-            if (std.mem.eql(u8, command_string, "v")) {
-                command = .vertex;
-            } else if (std.mem.eql(u8, command_string, "f")) {
-                command = .face;
-            } else if (std.mem.eql(u8, command_string, "g")) {
-                command = .group;
-            } else {
-                // Ignore invalid or unsupported commands silently.
-                obj.number_of_ignored_commands += 1;
-                continue :iterate_contents;
-            }
-
-            // Parse and handle command parameters.
-            var literal_start: usize = command_end + 1;
-            var literal_end: usize = undefined;
-            switch (command) {
-                .vertex => {
-                    var vertex_component: usize = 0;
-                    var vertex = tup.point(undefined, undefined, undefined);
-                    for (line[literal_start..]) |literal_character, literal_index| {
-                        if (literal_character == ' ' or literal_character == '\n') {
-                            literal_end = command_end + 1 + literal_index;
-                            vertex[vertex_component] = try std.fmt.parseFloat(
-                                f64,
-                                line[literal_start..literal_end]);
-                            literal_start = literal_end + 1;
-                            vertex_component += 1;
-                        }
-                    }
-
-                    if (vertex_component != 3) return ParserError.InvalidCommandArity;
-                    try obj.vertices.append(vertex);
+            switch (literal[0]) {
+                'v' => {
+                    const x = try std.fmt.parseFloat(f64, try getLiteral(&literals));
+                    const y = try std.fmt.parseFloat(f64, try getLiteral(&literals));
+                    const z = try std.fmt.parseFloat(f64, try getLiteral(&literals));
+                    try obj.vertices.append(tup.point(x, y, z));
                 },
-                .face => {
-                    var vertex_indeces = try std.ArrayList(usize).initCapacity(allocator, 3);
-                    defer vertex_indeces.deinit();
+                'f' => {
+                    var indices = std.ArrayList(usize).init(obj.allocator);
+                    defer indices.deinit();
 
-                    for (line[literal_start..]) |literal_character, literal_index| {
-                        if (literal_character == ' ' or literal_character == '\n') {
-                            literal_end = command_end + 1 + literal_index;
-                            const vertex_index = try std.fmt.parseInt(
-                                usize, line[literal_start..literal_end], 10);
-                            try vertex_indeces.append(vertex_index);
-                            literal_start = literal_end + 1;
-                        }
-                    }
-                    if (vertex_indeces.items.len < 3) return ParserError.InvalidCommandArity;
+                    while (literals.next()) |vertex_index|
+                        try indices.append(try std.fmt.parseInt(usize, vertex_index, 10));
 
-                    // Define list of faces to which to append this new face.
-                    // By default, assign it to the standard list of faces.
-                    // However, assign it the corresponding named group instead
-                    // if specified.
+                    // By default, append the new face to the standard list.
+                    // However, append the face to a named list given a group
+                    // name specified with the `g` command.
                     var faces: *std.ArrayList(Face) = &obj.faces;
                     if (group_name) |group| {
                         const entry = try obj.groups.getOrPut(try allocator.dupe(u8, group));
@@ -183,29 +126,21 @@ pub fn parseObjFile(allocator: std.mem.Allocator, file: std.fs.File) !ObjFile {
                         faces = entry.value_ptr;
                     }
 
-                    if (vertex_indeces.items.len > 3) {
-                        // A face with more than 3 vertices represents a
-                        // polygon. Because the ray tracer cannot render all
-                        // polygons directly, it's necessary to convert these
-                        // polygons into triangles.
-                        try triangulatePolygon(faces, obj.vertices.items, vertex_indeces.items);
+                    if (indices.items.len > 3) {
+                        try triangulatePolygon(faces, obj.vertices.items, indices.items);
                     } else {
                         try faces.append(Face.init(
-                            obj.vertices.items[vertex_indeces.items[0]],
-                            obj.vertices.items[vertex_indeces.items[1]],
-                            obj.vertices.items[vertex_indeces.items[2]]));
+                            obj.vertices.items[indices.items[0]],
+                            obj.vertices.items[indices.items[1]],
+                            obj.vertices.items[indices.items[2]]));
                     }
                 },
-                .group => {
-                    for (line[literal_start..]) |literal_character, literal_index| {
-                        if (literal_character == ' ' or literal_character == '\n') {
-                            literal_end = command_end + 1 + literal_index;
-                            group_name = line[literal_start..literal_end];
-                            break;
-                        }
-                    } else return ParserError.InvalidCommandArity;
-                },
+                'g' => group_name = try getLiteral(&literals),
+                else => return ParserError.InvalidCommand,
             }
+
+            // Report an unused literal in a command as an error.
+            if (literals.next() != null) return ParserError.InvalidCommand;
         }
     }
 
@@ -213,16 +148,20 @@ pub fn parseObjFile(allocator: std.mem.Allocator, file: std.fs.File) !ObjFile {
     return obj;
 }
 
+fn getLiteral(literals: *std.mem.TokenIterator(u8)) ![]const u8 {
+    return literals.next() orelse return ParserError.InvalidCommand;
+}
+
 /// Convert some convex polygon to a set of triangles, where a convex polygon
 /// is a polygon whose interior angles are all less than or equal to 180
 /// degrees.
-fn triangulatePolygon(faces: *std.ArrayList(Face), vertices: []Vertex, vertex_indeces: []usize) !void {
-    std.debug.assert(vertex_indeces.len > 3);
-    for (vertex_indeces[1..vertex_indeces.len]) |_, index| {
+fn triangulatePolygon(faces: *std.ArrayList(Face), vertices: []Vertex, vertex_indices: []usize) !void {
+    std.debug.assert(vertex_indices.len > 3);
+    for (vertex_indices[1..vertex_indices.len]) |_, index| {
         try faces.append(Face.init(
-            vertices[vertex_indeces[0]],
-            vertices[vertex_indeces[index]],
-            vertices[vertex_indeces[index + 1]]));
+            vertices[vertex_indices[0]],
+            vertices[vertex_indices[index]],
+            vertices[vertex_indices[index + 1]]));
     }
 }
 
